cbcf633397df49fc204895d50e52c74a00adc5b3
John Lam <jlam@iunknown.com>
2017-04-07 13:28:44 -0700

    clear screen on exit

--- kilo.c
+++ kilo.c
@@ -18,6 +18,9 @@ struct termios orig_termios;
 /*** terminal ***/
 
 void die(const char *s) {
+    write(STDOUT_FILENO, "\x1b[2J", 4);
+    write(STDOUT_FILENO, "\x1b[H", 3);
+
     perror(s);
     exit(1);
 }
@@ -61,6 +64,7 @@ char editorReadKey() {
 
 void editorRefreshScreen() {
     write(STDOUT_FILENO, "\x1b[2J", 4);
+    write(STDOUT_FILENO, "\x1b[H", 3);
 }
 
 /*** input ***/
@@ -70,8 +74,10 @@ void editorProcessKeypress() {
 
     switch (c) {
         case CTRL_KEY('q'):
-        exit(0);
-        break;
+            write(STDOUT_FILENO, "\x1b[2J", 4);
+            write(STDOUT_FILENO, "\x1b[H", 3);
+            exit(0);
+            break;
     }
 }
 
